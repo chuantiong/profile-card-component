9d4a907c83875d8903330fbe7f03f5e0495fdb2a
chuantiong <tiongchuan@gmail.com>
2023-12-17 15:23:55 +0800

    Commit changes

---
+++ index.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        title: "80K",
+        text: "Followers"
+    },
+    {
+        title: "803K",
+        text: "Likes"
+    },
+    {
+        title: "80K",
+        text: "Photos"
+    }
+];
+
+const getContentHtml = () => {
+    let contentHtml = "";
+    data.forEach(item => {
+        contentHtml += `
+        <div class="card__content">
+            <p class="card__title">${item.title}</p>
+            <p class="card__text">${item.text}</p>
+        </div>
+        `
+    });
+    return contentHtml;
+};
+
+const render = () => {
+    document.getElementById("card-contents").innerHTML = getContentHtml();
+};
+render()
